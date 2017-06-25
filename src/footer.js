f342aaa449e9d1858cc0e035df1d61e94a064a48
deeppatel234 <deeppatel234@live.com>
2017-06-25 22:37:03 +0530

    add footer

--- src/footer.js
+++ src/footer.js
@@ -7,22 +7,22 @@ class Footer extends Component {
           <footer className="page-footer bg-app center-on-small-only">
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-md-6">
-                        <h5 className="title">Footer Content</h5>
-                        <p>Here you can use rows and columns here to organize your footer content.</p>
+                    <div className="col-md-9">
+                        <h5 className="title">GUJARAT POWER ENGINEERING AND RESEARCH INSTITUTE</h5>
+                        <p>Department of Computer Engineering</p>
                     </div>
-                    <div className="col-md-6">
+                    <div className="col-md-3">
                         <h5 className="title">Links</h5>
                         <ul>
-                            <li><a href="#!">Link 1</a></li>
-                            <li><a href="#!">Link 2</a></li>
+                            <li><a href="https://www.gperi.ac.in">GPERI</a></li>
+                            <li><a href="https://www.gtu.ac.in">GTU</a></li>
                         </ul>
                     </div>
                 </div>
             </div>
             <div className="footer-copyright">
                 <div className="container-fluid">
-                    <i className="fa fa-copyright" aria-hidden="true"></i> 2017 Copyright: <a href="https://www.MDBootstrap.com"> Deep Patel </a>
+                    Developed & Design By <a target="_blank" href="https://www.linkedin.com/in/deeppatel234/"> Deep Patel </a>
                 </div>
             </div>
           </footer>
